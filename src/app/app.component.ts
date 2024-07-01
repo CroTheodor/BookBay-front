a3a4e68fd04d966bdb997898308ed639d13247d0
Theo <cro.theodor@gmail.com>
2024-07-02 00:11:44 +0200

    auth service login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,26 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { io } from "socket.io-client";
+import { AuthService } from './services/auth.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'bayb';
-  socket = io("localhost:4200");
+
+  constructor(
+    private authService: AuthService
+  ) {
+    this.testLogin();
+  }
+
+  private testLogin() {
+    this.authService.login('king@gmail.com', "test_password", false).subscribe((el) => console.log(el));
+  }
 }
--- src/app/services/listings.service.spec.ts
+++ src/app/services/listings.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthserviceService } from './authservice.service';
+import { ListingsService } from './listings.service';
 
-describe('AuthserviceService', () => {
-  let service: AuthserviceService;
+describe('ListingsService', () => {
+  let service: ListingsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthserviceService);
+    service = TestBed.inject(ListingsService);
   });
 
   it('should be created', () => {
