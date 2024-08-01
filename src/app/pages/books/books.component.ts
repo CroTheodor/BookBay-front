8c3e8428972fe3a8fb49b9a7fc5235d6e1db459c
unknown <cro.theodor@gmail.com>
2024-08-01 22:35:31 +0200

    filter for book list

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -3,18 +3,40 @@ import { ListingCardComponent } from '../../components/listing-card/listing-card
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { ListingsService } from '../../services/listings.service';
 import { Router } from '@angular/router';
-import { HttpResponse, PaginatedRequest, PaginatedResponse } from '../../interfaces/http.model';
+import {
+  HttpResponse,
+  PaginatedRequest,
+  PaginatedResponse,
+} from '../../interfaces/http.model';
 import { catchError, of } from 'rxjs';
-import { ListingDTO } from '../../interfaces/listing.model';
+import {
+  ListingDTO,
+  ListingRequestFilter,
+} from '../../interfaces/listing.model';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [ListingCardComponent, MatPaginatorModule],
+  imports: [
+    ListingCardComponent,
+    MatPaginatorModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './books.component.html',
   styleUrl: './books.component.scss',
 })
-export class BooksComponent implements OnInit{
+export class BooksComponent implements OnInit {
   length: number = 10;
   pageSize: number = 10;
   pageIndex: number = 0;
@@ -23,6 +45,19 @@ export class BooksComponent implements OnInit{
 
   pageEvent!: PageEvent;
   bookList: ListingDTO[] = [];
+
+  supportedFilters: string[] = ['-', 'Author', 'Title', 'Publisher', 'Course'];
+
+  selectedFilter: FormControl = new FormControl('', Validators.required);
+  filter: FormControl = new FormControl(
+    {
+      value: '',
+      disabled: true,
+    },
+    [Validators.required],
+  );
+
+  activeFilter: ListingRequestFilter = {};
 
   constructor(
     private listingService: ListingsService,
@@ -42,23 +77,62 @@ export class BooksComponent implements OnInit{
     this.getActiveListings();
   }
 
-  getActiveListings(){
-    const paginator: PaginatedRequest ={
-      page:this.pageIndex,
-      limit: this.pageSize
-    }
-    this.listingService.getActive(paginator)
-      .pipe(
-        catchError(err=>of(err))
-      ).subscribe(
-        (response: HttpResponse<PaginatedResponse<ListingDTO>>)=>{
-          this.length = response.response.totalItems;
-          this.bookList = response.response.content;
-        }
-      )
+  getActiveListings() {
+    const paginator: PaginatedRequest = {
+      page: this.pageIndex,
+      limit: this.pageSize,
+    };
+
+    this.listingService
+      .getActive(paginator, this.activeFilter)
+      .pipe(catchError((err) => of(err)))
+      .subscribe((response: HttpResponse<PaginatedResponse<ListingDTO>>) => {
+        this.length = response.response.totalItems;
+        this.bookList = response.response.content;
+      });
   }
 
-  goToDetails(id: string){
+  goToDetails(id: string) {
     this.router.navigate([`books/details/${id}`]);
+  }
+
+  filterChange(filter: string) {
+    this.filter.setValue('');
+    if (filter === '-') {
+      this.filter.disable();
+      if (Object.keys(this.activeFilter).length > 0) {
+        this.activeFilter = {};
+        this.getActiveListings();
+        this.selectedFilter.setValue('');
+      }
+    } else {
+      this.filter.enable();
+    }
+    this.activeFilter = {};
+  }
+
+  applyFilter() {
+    if (!this.filter.valid) {
+      return;
+    }
+    const filter: string = (this.filter.value as string).trim();
+    const selectedFilter: string = (this.selectedFilter.value as string)
+      .toLowerCase()
+      .trim();
+    this.pageIndex = 0;
+    this.activeFilter = {};
+    if (selectedFilter === 'publisher') {
+      this.activeFilter.publisher = filter;
+    }
+    if (selectedFilter === 'author') {
+      this.activeFilter.author = filter;
+    }
+    if (selectedFilter === 'title') {
+      this.activeFilter.title = filter;
+    }
+    if (selectedFilter === 'course') {
+      this.activeFilter.course = filter;
+    }
+    this.getActiveListings();
   }
 }
